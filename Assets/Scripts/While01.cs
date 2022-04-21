a83a307bda5fc4b45fb64d253fd91f9ec42cf401
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-04-21 15:34:12 -0300

    Ejercicios While 1 y 2 Terminados

---
+++ Assets/Scripts/While01.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+
+    public int counter;
+    public int countermax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(counter <= countermax)
+        {
+            Debug.Log(counter);
+            counter ++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/While02.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While02 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num2 >= num1)
+        {
+            Debug.Log("El primer número debe ser mayor al segundo");
+        }
+
+        else
+        {
+            while(num2 <= num1)
+            {
+                Debug.Log(num1);
+                num1--;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
